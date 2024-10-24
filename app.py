847af844bdfb86eaa827621994c55fdd70c06289
Majihone <oky2384800@stu.o-hara.ac.jp>
2024-10-24 14:16:05 +0900

    mama

--- app.py
+++ app.py
@@ -222,14 +222,12 @@ def inject_account_info():
         'account_name': session.get('account_name'),
         'account_id': session.get('account_id')
     }
-<<<<<<< HEAD
 
-=======
->>>>>>> 9b29d69b011144e47dccc7a29bfba34ea0a8372d
 @app.route('/mainmenu/mainmenu')
 def mainmenu():
     if 'account_name' in session:
-        return render_template('mainmenu/mainmenu.html')  # 引数がシンプルになった
+        breadcrumbs=[('メインメニュー', url_for('mainmenu'))]
+        return render_template('mainmenu/mainmenu.html',breadcrumbs=breadcrumbs)  # 引数がシンプルになった
     return redirect(url_for('login'))
 
 @app.route('/master/account_look')
@@ -268,11 +266,14 @@ def delete_account(account_id):
 
 @app.route('/photo/photo_menu')
 def photo_menu():
-    return render_template('photo/photo_menu.html')  # 引数がシンプルになった
+    breadcrumbs = [('メインメニュー', url_for('mainmenu')), ('フォトメニュー', url_for('photo_menu'))]
+    return render_template('photo/photo_menu.html', breadcrumbs=breadcrumbs)
+
 
 @app.route('/photo/photo_take')
 def photo_take():
-    return render_template('photo/photo_take.html')  # 引数がシンプルになった
+    breadcrumbs = [('メインメニュー', url_for('mainmenu')), ('フォトメニュー', url_for('photo_menu')), ('撮影画面', url_for('photo_take'))]
+    return render_template('photo/photo_take.html', breadcrumbs=breadcrumbs)  # 引数がシンプルになった
 
 # ーーーーーーーーーーアカウント設定ーーーーーーーーーー
 @app.route('/acset/acct_set')
