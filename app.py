fabf43dae34893d93a7c9e75bddabbf2d17c83f8
NekoSakana007 <oky2384824@stu.o-hara.ac.jp>
2024-11-26 10:48:57 +0900

    sa

--- app.py
+++ app.py
@@ -651,8 +651,6 @@ def save_gohan_post():
 
 #     return redirect(url_for('area_gohan'))
 
-import os
-
 @app.route('/hiroba/delete_post/<int:post_id>', methods=['POST'])
 def delete_post(post_id):
     conn = get_db()
